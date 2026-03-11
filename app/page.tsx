bfdd0977fe235c16dc10924b5d599aea936e3b14
Immonn <tgautam859@gmail.com>
2026-03-11 17:43:59 +0530

    Add some finishing

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import LandingNavbar from "@/app/components/LandingNavbar"
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebase/firebase';
@@ -9,12 +9,18 @@ import { useRouter } from 'next/navigation';
 //Navbar 5 rem so thats why 100vh-5rem
 const LandingPage= () => { 
     const [user,loading,error]=useAuthState(auth)
+    const [pageloading,setpageloading]=useState(true);
+
     const router=useRouter()
     useEffect(()=>{
         if (user){
             router.push('/dashboard')
         }
-    },[user,router])
+        if (!loading && !user) setpageloading(false);
+    },[user,router,loading])
+    if (pageloading){
+        return null;
+    }
     return <div className='bg-linear-to-b from-gray-600 to-black 
     h-screen relative'>
         <div className="max-w-7xl mx-auto">
