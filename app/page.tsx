09873c29d1736ed6a0302112796361e9551bc841
Immonn <tgautam859@gmail.com>
2026-03-12 18:28:05 +0530

    Remove that login logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,10 @@
-"use client"
-import React, { useEffect, useState } from 'react';
+
 import LandingNavbar from "@/app/components/LandingNavbar"
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from './firebase/firebase';
-import { useRouter } from 'next/navigation';
 
 
 //Navbar 5 rem so thats why 100vh-5rem
 const LandingPage= () => { 
-    const [user,loading,error]=useAuthState(auth)
-    const [pageloading,setpageloading]=useState(true);
 
-    const router=useRouter()
-    useEffect(()=>{
-        if (user){
-            router.push('/dashboard')
-        }
-        if (!loading && !user) setpageloading(false);
-    },[user,router,loading])
-    if (pageloading){
-        return null;
-    }
     return <div className='bg-linear-to-b from-gray-600 to-black 
     h-screen relative'>
         <div className="max-w-7xl mx-auto">
