02f5f0e4bd645264a989c1a552441b45b6bb8340
Immonn <tgautam859@gmail.com>
2026-03-20 14:53:49 +0530

    Add profile

--- app/components/DashboardNavbar.tsx
+++ app/components/DashboardNavbar.tsx
@@ -1,28 +1,38 @@
+'use client'
 import Link from 'next/link';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../firebase/firebase';
 
 
 type NavbarProps = {
-    
+
 };
 
-const DashboardNavbar:React.FC<NavbarProps> = () => {
-    
+const DashboardNavbar: React.FC<NavbarProps> = () => {
+    const [user] = useAuthState(auth)
     return <div className='flex items-center justify-between sm:px-12 px-2 md:px-24 '>
         <div className="flex pointer-events-none select-none items-center justify-center h-20 ">
-            <img src="/logo.png" alt='FixMyLogic' className='h-full'/>
+            <img src="/logo.png" alt='FixMyLogic' className='h-full' />
         </div>
         <div className="flex items-center gap-3">
             <Link href={"/auth/signup"}>
-            <button className='cursor-pointer border border-gray-300 text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
+                <button className='cursor-pointer border border-gray-300 text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
             hover:text-brand-orange hover:bg-white transition duration-300 ease-in-out'>Buymeacoffee</button>
             </Link>
-            <Link href={"/auth/signin"}>
-            <div className='cursor-pointer border border-gray-300 text-white px-2 py-1 sm:px-4 rounded-md text-sm font-medium
-            hover:text-brand-orange hover:bg-white transition duration-300 ease-in-out'>Your Profile</div>
-            </Link>
-            
+            {user && (
+                <div className='cursor-pointer group relative'>
+                    <img src='/avatar.png' alt='Avatar' width={30} height={30} className='rounded-full' />
+                    <div
+                        className='absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+								z-40 group-hover:scale-100 scale-0 
+								transition-all duration-300 ease-in-out'
+                    >
+                        <p className='text-sm'>{user.email}</p>
+                    </div>
+                </div>
+            )}
         </div>
-        </div>
+    </div>
 }
 export default DashboardNavbar;
