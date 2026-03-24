6ba839fd67d256a8b95330babec2eabd569e538d
Immonn <tgautam859@gmail.com>
2026-03-24 16:27:28 +0530

    Add google auth

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ const LandingPage= () => {
 
     return <div className='bg-linear-to-b from-gray-600 to-black 
     min-h-screen relative'>
-        <div className="max-w-7xl mx-auto flex flex-col h-screen">
+        <div className="max-w-7xl mx-auto flex flex-col min-h-screen">
             <LandingNavbar/>
             <div className='flex-1 flex items-center justify-center w-full pointer-events-none select-none px-4 sm:px-8 pb-10'>
-                <img src="/hero.png" alt='heroimage' className="w-[90%] sm:w-[85%] max-w-5xl object-contain drop-shadow-2xl scale-110" />
+                <img src="/hero.png" alt='heroimage' className="w-[95%] sm:w-[90%] max-w-5xl object-contain drop-shadow-2xl" />
             </div>
         </div>
     </div>
