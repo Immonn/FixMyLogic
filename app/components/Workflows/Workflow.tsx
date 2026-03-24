e508a0778cf838ca937415ad8e358b7e4bf2f578
Immonn <tgautam859@gmail.com>
2026-03-25 00:28:30 +0530

    Fix language issue

--- app/components/Workflows/Workflow.tsx
+++ app/components/Workflows/Workflow.tsx
@@ -4,6 +4,7 @@ import dynamic from 'next/dynamic';
 import ProblemDescription from './ProblemDescription';
 import Playground from './Playground';
 import { Problem } from '@/app/utils/types';
+import Confetti from 'react-confetti';
 const Split = dynamic(() => import('react-split'), { ssr: false });
 
 import { useHasMounted } from '@/app/hooks/useHasMounted';
@@ -25,6 +26,7 @@ const Workflow: React.FC<WorkflowProps> = ({ problem }) => {
         </div>
         <div className='h-full min-w-0 overflow-hidden'>
             <Playground problem={problem} setSuccess={setSuccess} setSolved={setSolved} success={success}/>
+            {success && <Confetti gravity={0.3} tweenDuration={4000} />}
         </div>
 
     </Split>
